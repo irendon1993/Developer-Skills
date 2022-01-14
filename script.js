6ff5237eea1e294ce557b4959424cf92b3e9015c
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-14 15:02:20 -0500

    Completed challenge

--- script.js
+++ script.js
@@ -72,24 +72,103 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 // console.log(amplitudeNew);
+// debugger;
 
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temp',
-    unit: 'celsius',
-    // C) FIX
-    value: Number(prompt('Degrees celsius')),
-  };
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     // C) FIX
 
-  // B) FIND
-  console.log(measurement);
+//     value: 10,
+//   };
 
-  //   console.log(measurement.value);
-  //   console.warn(measurement.value);
-  //   console.error(measurement.value);
+//   // B) FIND
+//   console.log(measurement);
 
-  const kelvin = measurement.value + 273;
-  return kelvin;
+//   //   console.log(measurement.value);
+//   //   console.warn(measurement.value);
+//   //   console.error(measurement.value);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// // A) IDENTIFY
+// console.log(measureKelvin());
+
+// // Using Debgugger
+// debugger;
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// // A) IDENTIFY
+// console.log(amplitudeBug);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Understand the problem
+// - print forecast with degrees
+
+// 2) Breaking up into sub-problems
+// - Add ... to messages
+// - Add days to messages
+//
+
+// debugger;
+const printForecast = function (arr) {
+  let dots = '... ';
+  let forecast = '';
+
+  const weekForecast = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let day = arr.length + 1 - (arr.length - i);
+    // B) FIND
+    // if (arr[i] >= 0) {
+    forecast = dots + arr[i] + `ºC in ${day} days `;
+    weekForecast.push(forecast);
+    // } else {
+    //   let negativeDay = -1 * arr[i];
+    //   forecast = dots + arr[i] + `ºC in ${negativeDay} days `;
+    //   weekForecast.push(forecast);
+    // }
+  }
+  return weekForecast;
 };
+
+// let testData = [17, 21, 23];
 // A) IDENTIFY
-console.log(measureKelvin());
+let testData = [12, 5, -5, 0, 4, 4, 4, 2, 4, 2, -5, -2, -5, -6, 43, 3];
+console.log(printForecast(testData));
+
+// TEST DATA 2: [12, 5, -5, 0, 4]
