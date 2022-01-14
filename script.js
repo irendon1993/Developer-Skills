57cc47e65fdab490764bcc89d5f1ffedcaa3bd54
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-14 11:13:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+/*
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day,
+ calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+ */
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem with temperature
+// What is temp amplitude? Answer: differnce between highest and lowest temp
+
+// 2 Breaking up into sub-problems
