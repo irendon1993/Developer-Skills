b6e6c742a1c8fe2699b8f0371209847796e2ed56
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-14 11:31:43 -0500

    onto to problem 2

--- script.js
+++ script.js
@@ -13,6 +13,37 @@
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem with temperature
-// What is temp amplitude? Answer: differnce between highest and lowest temp
+// - What is temp amplitude? Answer: differnce between highest and lowest temp
+// - How to compute max and min temperature? Answer:
+// - Whats a sensor error? And What to do?
 
 // 2 Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtrat min from max(amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+calcTempAmplitude([3, 7, 4, 'error', 5, 8]);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now recieve 2 arrays of temperatures
+
+// 1) understanding the problem
+// - With 2 arrays, wshould we implement
